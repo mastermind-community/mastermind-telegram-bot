3fa6881eefcfd040c50732b223c9c6e16b742f77
Serhii Yevtushok <ggazila@gmail.com>
2024-01-01 20:39:37 +0200

    removed daily tasks code

--- src/main/java/org/esadev/mastermindhelper/consts/Constants.java
+++ src/main/java/org/esadev/mastermindhelper/consts/Constants.java
@@ -5,9 +5,8 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
-    public static final String LEADERS_RANGE = "Dashboard!B2:C10";
+    public static final String LEADERS_RANGE = "Dashboard!B2:C12";
     public static final String SEND_DAILY_STATS = "0 0 6 * * *";
-    public static final String SEND_DAILY_TASK = "0 0 4 * * *";
     public static final String NO_LEADER_INFO_MESSAGE = "`З якоїсь причини не можу отримати інформацію. Зверніться в підтримку";
     public static final String LEADER_INFO_FIRST_LINE_MESSAGE = "**Статистика на %s**\n\n";
     public static final String LEADER_INFO_BUILD_LINE_MESSAGE = "`%s місце - %s => %s %s`\n";
--- src/main/java/org/esadev/mastermindhelper/scheduled/ScheduleNotifications.java
+++ src/main/java/org/esadev/mastermindhelper/scheduled/ScheduleNotifications.java
@@ -1,13 +1,7 @@
 package org.esadev.mastermindhelper.scheduled;
 
-import static org.esadev.mastermindhelper.consts.Constants.SEND_DAILY_STATS;
-import static org.esadev.mastermindhelper.consts.Constants.SEND_DAILY_TASK;
-
-import java.time.LocalDate;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.esadev.mastermindhelper.entity.dto.LeaderInfoDto;
-import org.esadev.mastermindhelper.service.DailyTaskService;
 import org.esadev.mastermindhelper.service.MessageFormatService;
 import org.esadev.mastermindhelper.service.SheetService;
 import org.esadev.mastermindhelper.service.TelegramSender;
@@ -15,6 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+import static org.esadev.mastermindhelper.consts.Constants.SEND_DAILY_STATS;
 
 @EnableScheduling
 @Service
@@ -24,23 +22,12 @@ public class ScheduleNotifications {
   private final TelegramSender telegramSender;
   private final SheetService sheetService;
   private final MessageFormatService messageFormatService;
-  private final DailyTaskService dailyTaskService;
 
   @Scheduled(cron = SEND_DAILY_STATS)
   public void sendSprintStatistics() throws TelegramApiException {
     List<LeaderInfoDto> info = sheetService.getLeadersInfo();
     String leadersInfo = messageFormatService.formatLeaderInfo(info);
     telegramSender.sendMessage(leadersInfo);
-  }
-
-  @Scheduled(cron = SEND_DAILY_TASK)
-  public void sendDailyTask() throws TelegramApiException {
-    if (LocalDate.now().isBefore(LocalDate.of(2024, 1, 1))) {
-      var task = dailyTaskService.choose();
-      if (task != null) {
-        telegramSender.sendMessage(task);
-      }
-    }
   }
 
 }
